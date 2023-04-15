e5fdfa998988a96e21f0e934728be786b338c277
Abbos <abbos887@mail.ru>
2023-04-15 13:44:01 +0500

    add ExceptionTranslator lib zolando problem

--- src/main/java/com/example/fileupload/repository/EmployeeRepository.java
+++ src/main/java/com/example/fileupload/repository/EmployeeRepository.java
@@ -1,7 +1,6 @@
 package com.example.fileupload.repository;
 
 import com.example.fileupload.domain.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
+public interface EmployeeRepository extends BaseRepository<Employee,Integer> {
 }
